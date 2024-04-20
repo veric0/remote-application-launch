eb0ece71de51b987142358b222e58e382a60623c
Dmytro Borak <borak.dmytro@gmail.com>
2024-04-21 00:56:35 +0300

    add simple client

--- src/remote_app_launch_client.c
+++ src/remote_app_launch_client.c
@@ -16,6 +16,12 @@ int main() {
         printf("Connection failed.\n");
         return 4;
     }
+
+
+    int pids[2] = {0}; // do linked list
+
+    // loop:
+
     int response_len = recv_message(clientSocket, buffer, sizeof(buffer));
     printf("response_len = %d\n", response_len);
     printf("Message received: %s\n", buffer);
@@ -23,6 +29,71 @@ int main() {
 //        printf("%c", buffer[i]);
 //    }
 //    printf("\n");
+
+    pids[0] = launch_process("echo ! hello from child 1", NULL);
+    pids[1] = launch_process("echo ! hello from child 2", NULL);
+    if (pids[0] == -1) {
+        printf("launch_process failed.\n");
+        return 6; // continue loop
+    }
+    for (int i = 0; i < 3; ++i) {
+        for (int p = 0; p < 2; ++p) {
+            if (pids[p] == 0) break;
+            switch (get_process_state(pids[p])) {
+                case 0:
+                    printf("process %d is still running.\n", pids[p]);
+                    break;
+                case 1:
+                    printf("process %d is exited.\n", pids[p]);
+                    pids[p] = 0;
+                    break;
+                case 2:
+                    printf("process %d is terminated.\n", pids[p]);
+                    pids[p] = 0;
+                    break;
+                default: // -1
+                    printf("get_process_status for %d is failed.\n", pids[p]);
+                    pids[p] = 0;
+                    break; // return
+            }
+        }
+        putchar('\n');
+        sleep_wrapper(1);
+    }
+
+    printf("terminate:\n");
+    if (terminate_process(pids[0]) == -1) {
+        printf("terminate failed.\n");
+        return 8;
+    } // else send response success terminate
+
+    for (int i = 0; i < 5; ++i) {
+        for (int p = 0; p < 2; ++p) {
+            if (pids[p] == 0) break;
+            switch (get_process_state(pids[p])) {
+                case 0:
+                    printf("process %d is still running.\n", pids[p]);
+                    break;
+                case 1:
+                    printf("process %d is exited.\n", pids[p]);
+                    pids[p] = 0;
+                    break;
+                case 2:
+                    printf("process %d is terminated.\n", pids[p]);
+                    pids[p] = 0;
+                    break;
+                default: // -1
+                    printf("get_process_status for %d is failed.\n", pids[p]);
+                    pids[p] = 0;
+                    break; // return
+            }
+        }
+        putchar('\n');
+        sleep_wrapper(1);
+    }
+
+    // end loop
+
     close_host_socket(clientSocket);
 
     return 0;
